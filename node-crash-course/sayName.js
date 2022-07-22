965b305fe64a25b68553fa8b7b733568ac3bc660
Gurik Mangat <gurikmangat8@gmail.com>
2022-07-22 13:41:08 -0700

    Add files via upload

---
+++ node-crash-course/sayName.js
@@ -0,0 +1,11 @@
+const sayName = () => {
+    console.log("Hello there Ed!");    
+};
+
+sayAdress =() => {
+    console.log("Johnson Street");
+}
+
+module.exports.sayName = sayName;
+module.exports.sayAdress = sayAdress;
+
